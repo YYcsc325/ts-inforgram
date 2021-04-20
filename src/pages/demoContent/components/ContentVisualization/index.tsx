df26b4e06e4ce1a16a89a70f247a79317544b1a4
huwz1128 <huwangzhe>
2021-04-20 22:11:22 +0800

    init:新增操作页

---
+++ src/pages/demoContent/components/ContentVisualization/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "./index.less";
+
+const ContentVisualization = () => {
+
+  return (
+    <div className={styles['contentVisualization']}>
+        <div>L</div>
+        <div>R</div>
+    </div>
+  );
+};
+
+export default ContentVisualization;
