c680fa42f06002b78a238ae3dfedbef7cb0ed58a
yycsc <m13625744973@163.com>
2021-04-24 15:19:56 +0800

    refactor: analytics页面完成

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -21,7 +21,34 @@ const Analytics: FC<IAnalyticsProps> = ({ consumer }) => {
 
   return (
     <div className={prefixCls()} onClick={handleClick}>
-      analytics
+      <div className={prefixCls("top-warp")}>
+        <div className={prefixCls("top")}>
+          <img
+            className={prefixCls("t-img")}
+            src="https://cdn.jifo.co/js/dist/371cffe65a286109717147b4016a1b94.png"
+            alt=""
+          />
+          <div className={prefixCls("t-engagement")}>Engagement analytics</div>
+          <div className={prefixCls("t-message")}>
+            Upgrade now to get detailed data from published projects and see
+            what resonates with your audience.
+          </div>
+          <div className={prefixCls("t-btn")}>
+            <a href="">Upgrade now</a>
+          </div>
+          <div className={prefixCls("t-jump")}>
+            <a href="https://infogram.zendesk.com/hc/en-us/articles/360008797013-Engagement-Analytics">
+              Find more about analytics
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={prefixCls("bottom")}>
+        <img
+          src="https://cdn.jifo.co/js/dist/aec0c93ebc0798c323261afedb8aa7ab.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
