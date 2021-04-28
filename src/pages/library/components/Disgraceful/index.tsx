9b9973392e87ec8183b645de32ce389014d5ebf3
yycsc <m13625744973@163.com>
2021-04-28 23:09:45 +0800

    refactor: 增加loading效果

--- src/pages/library/components/Disgraceful/index.tsx
+++ src/pages/library/components/Disgraceful/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useState, useCallback } from "react";
-import { createPrefixClass } from "@/util/utils";
 import { Checkbox } from "antd";
-import { EditOutlined, EllipsisOutlined } from "@ant-design/icons";
 import classNames from "classnames";
+import { createPrefixClass } from "@/util/utils";
+import React, { FC, useState, useCallback } from "react";
+import { EditOutlined, EllipsisOutlined } from "@ant-design/icons";
+import LazyLoadingImg from "@/components/LazyLoadingImg";
 
 import styles from "./index.less";
 
@@ -36,50 +37,53 @@ const Disgraceful: FC<IDisgracefulProps> & {
   );
 
   return (
-    <div className={classNames(prefixCls(), className)} onClick={handleClick}>
-      <div
-        className={prefixCls("disgrace-ful")}
-        style={{
-          backgroundImage: `url(${url})`,
-        }}
-        onMouseEnter={() => {
-          setIsEnter(true);
-        }}
-        onMouseLeave={() => {
-          setIsEnter(false);
-        }}
-      >
+    <LazyLoadingImg
+      className={classNames(prefixCls(), className)}
+      onClick={handleClick}
+      url={url}
+    >
+      <div>
         <div
-          className={classNames(prefixCls("disgrace"), {
-            [prefixCls("disgrace-hover")]: isEnter,
-          })}
+          className={prefixCls("disgrace-ful")}
+          onMouseEnter={() => {
+            setIsEnter(true);
+          }}
+          onMouseLeave={() => {
+            setIsEnter(false);
+          }}
         >
-          {!isEnter && <div className={prefixCls("title")}>Public</div>}
-          {(isEnter || checked) && (
-            <div className={prefixCls("title-checkbox")}>
-              <Checkbox onClick={handleCheck} checked={checked} />
-            </div>
-          )}
-          {isEnter && (
-            <div className={prefixCls("hover-title")}>
-              <DisgracefulIcon>
+          <div
+            className={classNames(prefixCls("disgrace"), {
+              [prefixCls("disgrace-hover")]: isEnter,
+            })}
+          >
+            {!isEnter && <div className={prefixCls("title")}>Public</div>}
+            {(isEnter || checked) && (
+              <div className={prefixCls("title-checkbox")}>
+                <Checkbox onClick={handleCheck} checked={checked} />
+              </div>
+            )}
+            {isEnter && (
+              <div className={prefixCls("hover-title")}>
+                <DisgracefulIcon>
+                  <EditOutlined />
+                </DisgracefulIcon>
+                <DisgracefulIcon style={{ marginLeft: "10px" }}>
+                  <EllipsisOutlined />
+                </DisgracefulIcon>
+              </div>
+            )}
+            {isEnter && <div className={prefixCls("edit")}>Edit</div>}
+            <div className={prefixCls("footer")}>
+              <div>{name}</div>
+              <div>
                 <EditOutlined />
-              </DisgracefulIcon>
-              <DisgracefulIcon style={{ marginLeft: "10px" }}>
-                <EllipsisOutlined />
-              </DisgracefulIcon>
-            </div>
-          )}
-          {isEnter && <div className={prefixCls("edit")}>Edit</div>}
-          <div className={prefixCls("footer")}>
-            <div>{name}</div>
-            <div>
-              <EditOutlined />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </LazyLoadingImg>
   );
 };
 
@@ -116,33 +120,3 @@ const DisgracefulIcon: FC<IDisgracefulIconProps> = ({
 Disgraceful.Icon = DisgracefulIcon;
 
 export default Disgraceful;
-
-const data = [
-  {
-    title: "中国",
-    id: "1",
-    children: [
-      {
-        title: "北京",
-        id: "1-1",
-      },
-      {
-        title: "上海",
-        id: "1-2",
-      },
-    ],
-  },
-];
-
-const option = [
-  {
-    title: "中国-北京",
-    id: "1-1",
-  },
-  {
-    title: "中国-上海",
-    id: "1-2",
-  },
-];
-
-option.find((item) => item.title);
