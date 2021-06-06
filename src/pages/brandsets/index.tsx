db5ce18a9a82c70f6e4667228510b0705b615c9e
yycsc <m13625744973@163.com>
2021-06-06 21:17:49 +0800

    refactor: 拖拽组件支持删除操作

--- src/pages/brandsets/index.tsx
+++ src/pages/brandsets/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useEffect } from "react";
 import { contextConsumer } from "@/layouts/context";
 import { createPrefixClass } from "@/util/utils";
-import ContextMenu from "@/components/DragContainer/ContextMenu";
 
 import styles from "./index.less";
 
@@ -22,9 +21,7 @@ const Brandsets: FC<IBrandsetsProps> = ({ consumer }) => {
 
   return (
     <div className={prefixCls()} onClick={handleClick}>
-      <ContextMenu options={[{ title: "测试数据1", value: "delete" }]}>
-        <div>这是什么</div>
-      </ContextMenu>
+      brandsets
     </div>
   );
 };
